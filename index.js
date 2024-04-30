df247167f5121f12d4f1d19cc50cda0b3e8821d4
rafiul-razib <rafiul.rzb1@gmail.com>
2024-04-30 19:05:22 +0600

    dotenv added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://rafiulrazib:nWQTNeNjH4eZ5cqi@cluster0.gplglww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gplglww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -54,7 +56,40 @@ async function run() {
       const result = await craftsCollection.deleteOne(query);
       res.send(result);
     })
-    
+
+    // image,
+    //   item_name,
+    //   subcategory_name,
+    //   short_description,
+    //   price,
+    //   rating,
+    //   customization,
+    //   processing_time,
+    //   stock_status,
+
+    app.put("/update/:id", async(req, res)=>{
+      const id = req.params.id;
+      const craft = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const updatedCraft = {
+        $set:{
+          image: craft.image,
+          item_name : craft.item_name,
+          subcategory_name : craft.subcategory_name,
+          short_description: craft.short_description,
+          price : craft.price,
+          rating : craft.rating,
+          customization: craft.customization,
+          processing_time: craft.processing_time,
+          stock_status: craft.stock_status
+        }
+      }
+
+      const option = {upsert : true};
+      const result = await craftsCollection.updateOne(filter, updatedCraft, option);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
